faca1b50cd06e354cdaf963172b8b4ec06af37f1
Isabella Barto <isabellabartovieira701@gmail.com>
2023-09-04 19:52:47 -0300

    ap02(impput-output) declaração variaveis

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+//pacote padrão: java.lang: importado automaticamente
+public class SomaDoisNumeros{
+    plublic static void main(String [] args) {
+        //decvlaração de variável
+        Scanner leitor = new Scanner(System.in);
+        double primeiroOperador;
+        double segundoOperador;
+        double resultado;
+
+        //entrada
+        System.out.println("Digite o primeiro operando");
+        primeiroOperando = leitor.nextDouble();
+        System.out.println("Digite o segundo operando");
+        segundoOperando = leitor.nextDoble();
+        resultado = leitor.nextDouble();
+        //processamento
+        resultado = primeiroOperando + segundoOperando;
+        //saída
+    }
+}
